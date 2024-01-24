adc41bb5c1b624bd375ef57974be793333ffab48
cardiscardis <giftedcardis@outlook.com>
2024-01-24 08:14:56 +0100

    frontend complete1

--- src/components/services/clientService.tsx
+++ src/components/services/clientService.tsx
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const client = axios.create({
-    baseURL: 'https://gelato-backend-fo49-48ybe5vxh-cardiscardis.vercel.app',
-    timeout: 2000
+    baseURL: 'https://gelato-backend-fo49.vercel.app/',
+    // timeout: 2000
 });
 
 
